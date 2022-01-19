debb744e945cde248e4d93ff28da7cce63305e94
Alexey Potapov <2furius@gmail.com>
2022-01-19 14:34:18 +0300

    pep8 fix

--- bot_web_board/views.py
+++ bot_web_board/views.py
@@ -1,15 +1,17 @@
 import logging
-from django.shortcuts import render, HttpResponse, HttpResponsePermanentRedirect
+from django.shortcuts import render, HttpResponsePermanentRedirect
+
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 # Create your views here.
 def index_bot(request):
-    logging.info('index')
-    return render(request, 'bot_web_board/tg_login.html')
+    logging.info("index")
+    return render(request, "bot_web_board/tg_login.html")
 
 
 def tg_login(request):
     logging.info("Login detected")
     logging.info(request)
-    return HttpResponsePermanentRedirect('/')
+    return HttpResponsePermanentRedirect("/")
