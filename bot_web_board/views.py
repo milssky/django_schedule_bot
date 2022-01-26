927955eb6e29d860873dd52d6c60ed9c7c4e96bd
milssky <2furius@gmail.com>
2022-01-26 15:02:25 +0300

    Добавлена функция валидации логина с серверов TG

--- bot_web_board/views.py
+++ bot_web_board/views.py
@@ -1,15 +1,19 @@
 import logging
 from django.shortcuts import render, HttpResponse, HttpResponsePermanentRedirect
 
+from .logic import validate_auth_data
+
 logging.basicConfig(level=logging.DEBUG)
 
-# Create your views here.
+
 def index_bot(request):
-    logging.info('index')
     return render(request, 'bot_web_board/tg_login.html')
 
 
 def tg_login(request):
-    logging.info("Login detected")
-    logging.info(request)
+    auth_data = dict(request.GET.items())
+    if not validate_auth_data(auth_data):
+        logging.error(f'Wrong login with {auth_data}')
+        return HttpResponse('Wrong auth data. Check it and try again')
+
     return HttpResponsePermanentRedirect('/')
