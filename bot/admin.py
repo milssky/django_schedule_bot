788991b216a44da3cf6cdc6fd253510cd69abca9
milssky <2furius@gmail.com>
2022-02-05 19:28:52 +0300

    Чистка

--- bot/admin.py
+++ bot/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+
 from .models import Task
-User = get_user_model()
-# Register your models here.
+
 
 admin.site.register(Task)
